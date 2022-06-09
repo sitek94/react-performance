ce5150f03b11f66df1b9b2305b7082a33be36bb3
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-06-09 06:47:41 +0200

    ex 6.2 - limit the work consuming components do

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -107,6 +107,11 @@ Grid = React.memo(Grid)
 function Cell({row, column}) {
   const state = useAppState()
   const cell = state.grid[row][column]
+  return <CellImpl cell={cell} row={row} column={column} />
+}
+Cell = React.memo(Cell)
+
+function CellImpl({cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -122,7 +127,7 @@ function Cell({row, column}) {
     </button>
   )
 }
-Cell = React.memo(Cell)
+CellImpl = React.memo(CellImpl)
 
 function DogNameInput() {
   const [{dogName}, dispatch] = useDogState()
